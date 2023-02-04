34684cea43f3504cdfd376232666a70b9fc746df
shancw <shancw1996@gmail.com>
2023-02-04 21:10:13 +0800

    feat: 充值中心首页静态样式

---
+++ src/views/business/front/recharge/components/recharge-goods-buy.vue
@@ -0,0 +1,49 @@
+<template>
+  <a-table :columns="columns" :data-source="data" bordered>
+    <template #title>购买记录</template>
+    <template #bodyCell="{ column, text }">
+      <template v-if="column.dataIndex === 'name'">
+        <a>{{ text }}</a>
+      </template>
+    </template>
+  </a-table>
+</template>
+<script setup>
+  import { ref } from 'vue';
+
+  let columns = ref([
+    {
+      title: 'Name',
+      dataIndex: 'name',
+    },
+    {
+      title: 'Cash Assets',
+      className: 'column-money',
+      dataIndex: 'money',
+    },
+    {
+      title: 'Address',
+      dataIndex: 'address',
+    },
+  ]);
+  const data = ref([
+    {
+      key: '1',
+      name: 'John Brown',
+      money: '￥300,000.00',
+      address: 'New York No. 1 Lake Park',
+    },
+    {
+      key: '2',
+      name: 'Jim Green',
+      money: '￥1,256,000.00',
+      address: 'London No. 1 Lake Park',
+    },
+    {
+      key: '3',
+      name: 'Joe Black',
+      money: '￥120,000.00',
+      address: 'Sidney No. 1 Lake Park',
+    },
+  ]);
+</script>
--- src/views/business/front/recharge/recharge-dashboard.vue
+++ src/views/business/front/recharge/recharge-dashboard.vue
@@ -1,3 +1,34 @@
 <template>
-  <h1>充值中心 - 列表页</h1>
+  <div class="p-4">
+    <div class="card-header rounded-md">
+      <div class="text-5xl font-bold">¥10.39</div>
+      <div>账号可用余额</div>
+      <p class="py-4">描述信息描述信息描述信息描述信息描述信息描述信息描述信息描述信息描述信息描述信息</p>
+      <div class="grid grid-cols-1 gap-6 w-1/6">
+        <a-button ghost shape="round">支付宝</a-button>
+        <a-button ghost shape="round">微信</a-button>
+        <a-button ghost shape="round">礼品卡</a-button>
+      </div>
+    </div>
+    <div class="mt-10 grid grid-cols-2 gap-2">
+      <RechargeHistory />
+      <RechargeGoodsBuy />
+    </div>
+  </div>
 </template>
+<script setup>
+  import RechargeHistory from '/@/views/business/front/recharge/components/recharge-history.vue';
+  import RechargeGoodsBuy from '/@/views/business/front/recharge/components/recharge-goods-buy.vue';
+</script>
+
+<style scoped>
+  .card-header {
+    padding: 40px;
+    background-image: linear-gradient(to bottom, #fb966e, #95a0f4);
+    color: #fff;
+    overflow: hidden;
+    height: auto;
+    min-height: auto;
+    display: block;
+  }
+</style>
